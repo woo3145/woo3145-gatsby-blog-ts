8e29308160fa61661fb94239031b850a925d54a4
Changwoo Lee <lcwoo31456@gmail.com>
2022-05-12 15:17:23 +0900

    Header - 다크모드 토글 추가

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,6 +6,19 @@ import "./index.scss";
 
 const Header = () => {
   const { title } = useSiteMetadata();
+
+  const darkModeToggle = () => {
+    const element = document.getElementById("dark-mode-btn-inner");
+    if (element) {
+      if (element.classList.contains("active")) {
+        element.classList.remove("active");
+        document.documentElement.setAttribute("data-theme", "light");
+      } else {
+        element.classList.add("active");
+        document.documentElement.setAttribute("data-theme", "dark");
+      }
+    }
+  };
   return (
     <header className="header-wrapper">
       <div className="header">
@@ -21,6 +34,9 @@ const Header = () => {
           <Link to="/posts" className="link">
             posts
           </Link>
+          <div className="dark-mode-btn" onClick={darkModeToggle}>
+            <div className="dark-mode-btn-inner" id="dark-mode-btn-inner"></div>
+          </div>
         </div>
       </div>
     </header>
